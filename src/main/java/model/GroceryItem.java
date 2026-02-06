e28e5981c65cf633432374e3663ec30998364fe1
Maximilan Waldenfeldt Uggla <maxi.uggla@icloud.com>
2026-02-06 18:31:30 +0100

    changed grocery iteam class to a record class

--- src/main/java/model/GroceryItem.java
+++ src/main/java/model/GroceryItem.java
@@ -1,15 +1,9 @@
 package model;
 
-public class GroceryItem {
-    private final String name;
-    private final String category;
+public record GroceryItem(String name, String category) {
 
-    public GroceryItem(String name, String category) {
-        this.name = name;
-        this.category = category;
+    @Override
+    public String toString() {
+        return name;
     }
-
-    public String getName() {return name;}
-
-    public String getCategory() {return category;}
 }
