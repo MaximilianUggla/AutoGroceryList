fa1939b15410b181fa99e67ca8fb98d9a9d0c60e
Maximilan Waldenfeldt Uggla <maxi.uggla@icloud.com>
2026-02-06 22:26:30 +0100

    Made changes to the search tree class so now it handles searches for grocery items and returns a set of all items with a prefix matching a given one. (Auto complete)

--- src/main/java/SearchTree/TrieTree.java
+++ src/main/java/SearchTree/TrieTree.java
@@ -1,35 +1,54 @@
 package SearchTree;
 
+import model.GroceryItem;
+
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class TrieTree {
     TrieNode root = new TrieNode();
 
-    public void insert(String prefix) {
+    public void insert(GroceryItem item) {
         TrieNode node = root;
-        for (Character c : prefix.toCharArray()) {
+        for (Character c : item.name().toCharArray()) {
             if (!node.children.containsKey(c)) {
                 TrieNode newNode = new TrieNode();
                 node.children.put(c, newNode);
             }
             node = node.children.get(c);
-            node.counter += 1;
         }
+        node.isItem = true;
+        node.category = item.category();
     }
 
-    public int startsWith(String prefix) {
+    public Set<GroceryItem> startsWith(String prefix) {
         TrieNode node = root;
         for (Character c : prefix.toCharArray()) {
             if (node.children.containsKey(c)) {
                 node = node.children.get(c);
-            } else {return 0;}
+            } else {return new HashSet<>();}
         }
-        return node.counter;
+        return searchHits(prefix, node.children, new HashSet<>());
     }
 
-    private class TrieNode {
-        private Map<Character, TrieNode> children = new HashMap<>();
-        private int counter = 0;
+    private Set<GroceryItem> searchHits(String prefix, Map<Character, TrieNode> children, Set<GroceryItem> results) {
+        for (Map.Entry<Character, TrieNode> entry : children.entrySet()) {
+            String newPrefix = prefix + entry.getKey();
+            TrieNode currentNode = entry.getValue();
+
+            if (currentNode.isItem) {
+                results.add(new GroceryItem(newPrefix, currentNode.category));
+            }
+            searchHits(newPrefix, currentNode.children, results);
+        }
+        return results;
+    }
+
+    private static class TrieNode {
+        private final Map<Character, TrieNode> children = new HashMap<>();
+        private boolean isItem = false;
+        private String category;
     }
 }
