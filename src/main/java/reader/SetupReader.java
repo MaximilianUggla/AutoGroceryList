daec3ae02964708796b8d3ef343f6d6aa18c5cb7
Maximilan Waldenfeldt Uggla <maxi.uggla@icloud.com>
2026-02-07 22:37:31 +0100

    updated setup reader to use more fitting data structures

--- src/main/java/reader/SetupReader.java
+++ src/main/java/reader/SetupReader.java
@@ -1,22 +1,25 @@
 package reader;
 
 import model.GroceryItem;
-
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class SetupReader {
 
-    public static Map<String, List<GroceryItem>> setup() throws FileNotFoundException {
-        Map<String, List<GroceryItem>> lists = new HashMap<>();
+    public static Set<GroceryItem>[] setup() throws FileNotFoundException, InterruptedException, TimeoutException {
         String startPath = "src/main/resources/";
-        String endPath = ".txt";
+
         List<String> fileNames = new ArrayList<>();
         Scanner scanner = new Scanner(new File(startPath + "PATHS.txt"));
-
         while (scanner.hasNextLine()) {
             String in = scanner.nextLine();
             fileNames.add(in);
@@ -25,20 +28,27 @@ public class SetupReader {
 
         ExecutorService executorService = Executors.newFixedThreadPool(4);
 
+        Set<GroceryItem>[] supplyMatrix = (Set<GroceryItem>[]) new HashSet[fileNames.size()];
+
         for (String fileName : fileNames) {
             executorService.submit(() -> {
                 Scanner scan = null;
-                try {scan = new Scanner(new File(startPath + fileName + endPath));} catch (FileNotFoundException e) {throw new RuntimeException(e);}
+                try {scan = new Scanner(new File(startPath + fileName + ".txt"));} catch (FileNotFoundException e) {throw new RuntimeException(e);}
 
-                List<GroceryItem> list = new ArrayList<>();
+                Set<GroceryItem> items = new HashSet<>();
                 while (scan.hasNextLine()) {
-                    list.add(new GroceryItem(fileName, scan.nextLine()));
+                    items.add(new GroceryItem(scan.nextLine(), fileName));
                 }
                 scan.close();
-                lists.put(fileName, list);
+                supplyMatrix[fileNames.indexOf(fileName)] = items;
             });
         }
 
-        return lists;
+        executorService.shutdown();
+        if (executorService.awaitTermination(1, TimeUnit.HOURS)) {
+            return supplyMatrix;
+        }  else {
+            throw new TimeoutException("Tasks timed out");
+        }
     }
 }
